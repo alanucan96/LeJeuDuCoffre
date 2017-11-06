45a1c108cbda6b89520f271f0d183d270ab87def
alanucan96 <alanvucan@gmail.com>
2017-11-06 16:44:25 +0100

    Cadena

--- src/Cadena.java
+++ src/Cadena.java
@@ -1,4 +1,15 @@
+import java.util.Vector;
+
+import javax.swing.JButton;
 
 public class Cadena {
 
+	private Boolean etat;
+	private int numero;
+	private JButton button;
+	private Vector vectorRelation;
+	
+	public Cadena(Boolean etat, int numero) {
+		
+	}
 }
