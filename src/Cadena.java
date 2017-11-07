55a84607ff31018589ce13a533918440dd51b46b
alanucan96 <alanvucan@gmail.com>
2017-11-07 21:30:27 +0100

    cadena

--- src/Cadena.java
+++ src/Cadena.java
@@ -1,6 +1,8 @@
 import java.util.Vector;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class Cadena {
 
@@ -8,8 +10,34 @@ public class Cadena {
 	private int numero;
 	private JButton button;
 	private Vector vectorRelation;
+	private ImageIcon cadenaFerme, cadenaOuvert;
+	private JLabel imageCadena;
 	
 	public Cadena(Boolean etat, int numero) {
-		
+		this.etat = etat;
+		this.numero = numero;
+		imageCadena = new JLabel();
+		if(etat == true) {
+			imageCadena.setIcon(cadenaOuvert);
+		}
+		else {
+			imageCadena.setIcon(cadenaFerme);
+		}
+	}
+	
+	public Boolean getEtat() {
+		return this.etat;
+	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void ouvrirCadena() {
+		imageCadena.setIcon(cadenaOuvert);
+	}
+	
+	public void fermerCadena() {
+		imageCadena.setIcon(cadenaFerme);
 	}
 }
