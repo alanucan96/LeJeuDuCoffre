2e48b3fd0351659489f283fcfbf59063c92bda6c
jbentayou <33425570+jbentayou@users.noreply.github.com>
2017-11-08 11:17:11 +0100

    Update Partie.java

--- src/Partie.java
+++ src/Partie.java
@@ -8,7 +8,7 @@ public class Partie {
 	    // Ouvrir le XML "'d'.xml"
     
 	    // déclaration d'un fichier Java standard
-	    File fichier =  new File("'d'.xml") ;
+	    File fichier =  new File(d + ".xml") ;
 	    // création d'un objet de type SAXReader 
 	    SAXReader reader =  new SAXReader() ;
 	    // lecture de ce fichier à l'aide de ce reader, et construction d'un objet Document
